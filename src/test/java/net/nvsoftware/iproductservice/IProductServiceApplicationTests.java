32cd25f2a54142d55cf3ec624255e09c81be51df
sqxsss <1514713140@qq.com>
2023-07-05 18:34:25 -0400

    Initial commit iProductService

---
+++ src/test/java/net/nvsoftware/iproductservice/IProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.iproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
